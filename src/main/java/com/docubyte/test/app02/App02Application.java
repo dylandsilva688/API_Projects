366a027ba059c3b5f8e2542e51327114373bcb4b
dylandsilva688 <66080245+dylandsilva688@users.noreply.github.com>
2025-10-17 17:24:41 +0530

    Update App02Application.java

--- src/main/java/com/docubyte/test/app02/App02Application.java
+++ src/main/java/com/docubyte/test/app02/App02Application.java
@@ -2,12 +2,18 @@ package com.docubyte.test.app02;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class App02Application {
+public class App02Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(App02Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(App02Application.class, args);
     }
-
 }
